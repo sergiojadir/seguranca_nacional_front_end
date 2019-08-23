8837608081d53eed088bd15d68e1032f5e3c97d3
Sergio Jadir Ribeiro Leite <jadirleite@unimedrj.coop.br>
2019-08-23 00:48:46 -0300

    Criar Frontend da aplicação (React/Redux) - Part 1.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Tickets from './components/Tickets';
+import Header from './components/Header';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+library.add(faCheckCircle, faTrashAlt)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Container>
+        <Tickets/>
+      </Container>
     </div>
   );
 }
